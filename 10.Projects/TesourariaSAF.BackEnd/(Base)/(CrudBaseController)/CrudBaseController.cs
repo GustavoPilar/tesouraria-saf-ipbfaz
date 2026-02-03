42ddc1252e71fc304b6cf5caf530d0b35269b00a
Gustavo Pilar <gustavo.silva@weipa.com.br>
2026-02-03 14:27:48 -0300

    Feat: GoogleSheetsService

---
+++ 10.Projects/TesourariaSAF.BackEnd/(Base)/(CrudBaseController)/CrudBaseController.cs
@@ -0,0 +1,41 @@
+﻿using Google.Apis.Sheets.v4.Data;
+using Microsoft.AspNetCore.Mvc;
+using TesourariaSAF.BackEnd.Services;
+
+namespace TesourariaSAF.BackEnd
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class CrudBaseController : ControllerBase
+    {
+        #region Fields
+        private readonly GoogleSheetsService _googleSheetsService;
+        #endregion
+
+        #region Constructor
+        public CrudBaseController(GoogleSheetsService googleSheetsService)
+        {
+            _googleSheetsService = googleSheetsService;
+        }
+        #endregion
+
+        #region Members 'Actions' :: HttpGet, HttpPost, HttpPut, HttpDelete
+
+        #region Get
+        [HttpGet("read")]
+        public IActionResult Get()
+        {
+            string sheets = "126bbypMATPCiy0dABy2iUXCBBjDFsRUw4kCBXaAkTBM";
+            string range = "Página1!A:C";
+
+            var request = _googleSheetsService.GetService().Spreadsheets.Values.Get(sheets, range);
+
+            ValueRange result = request.Execute();
+
+            return Ok(result);
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ 10.Projects/TesourariaSAF.BackEnd/Services/GoogleSheetsService.cs
@@ -0,0 +1,40 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+
+namespace TesourariaSAF.BackEnd.Services
+{
+    public class GoogleSheetsService
+    {
+        #region Fields
+        private readonly SheetsService _sheetsService;
+        #endregion
+
+        #region Constructor
+        public GoogleSheetsService()
+        {
+            GoogleCredential credential;
+
+            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream)
+                    .CreateScoped(SheetsService.Scope.Spreadsheets);
+            }
+
+            _sheetsService = new SheetsService(
+                new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Tesouraria SAF - IPBFaz"
+                });
+        }
+        #endregion
+
+        #region Getters :: GetService()
+        public SheetsService GetService()
+        {
+            return _sheetsService; 
+        }
+        #endregion
+    }
+}
